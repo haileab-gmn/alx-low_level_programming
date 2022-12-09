4012fbe955fc61f7d735f8e6cde59529bf7c4934
haileab-gmn <haileabgebremedhin64@gmail.com>
2022-12-09 05:01:09 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printif("size of char: %lu byte(s)\n",sizeof(char));
-	printif("size of int: %lu byte(s)\n",sizeof(int));
-	printif("size of long int: %lu byte(s)\n",sizeof(long int));
-	printif("size of long long int: %lu  byte(s)\n",sizeof(long long int));
-	printif("size of float: %lu  byte(s)\n",sizeof(float));
+	printif("size of a char: %lu byte(s)\n",sizeof(char));
+	printif("size of an int: %lu byte(s)\n",sizeof(int));
+	printif("size of a long int: %lu byte(s)\n",sizeof(long int));
+	printif("size of a long long int: %lu  byte(s)\n",sizeof(long long int));
+	printif("size of a float: %lu  byte(s)\n",sizeof(float));
 	return (0);
 }
