54aa161aed176256f816ed70be50c450c3fe33cc
haileab-gmn <haileabgebremedhin64@gmail.com>
2022-12-09 03:01:06 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle" );
-
-	return 0;
+	puts("\"programming is like writing a multilingual puzzle");
+	return (0);
 }
