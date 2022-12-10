0166376167dc69b9b44ee14706f485cc1ac71156
haileab-gmn <haileabgebremedhin64@gmail.com>
2022-12-10 09:13:36 -0800

    fprint

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
