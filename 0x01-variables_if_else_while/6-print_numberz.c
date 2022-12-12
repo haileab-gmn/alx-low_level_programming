c23ccab4461b6e31c63659e51d6a6dd9de150fd5
haileab-gmn <haileabgebremedhin64@gmail.com>
2022-12-12 12:02:18 -0800

    putchar with ints

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,8 +8,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
+	i = 0;
+	while
+		(i < 10)
+		{
+			putchar(i + '0');
+			i++;
+		}
+	putchar('\n');
 	return (0);
 }
