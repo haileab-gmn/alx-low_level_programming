01d863077a4aeb55a82b2828d1398dec1c687832
haileab-gmn <haileabgebremedhin64@gmail.com>
2022-12-12 02:55:03 -0800

    upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return always 0 (Success)
+ */
+int main(void)
+{
+        char c;
+
+	char u;
+
+	c = 'a';
+	u = 'A';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(u <= 'Z')
+		{
+			putchar(u);
+			u++;
+		}
+	putchar('n');
+        return (0);
+}
+
