f6a0d48973b50ac72069cdcec5c567a788225d04
haileab-gmn <haileabgebremedhin64@gmail.com>
2022-12-14 13:49:16 -0800

    prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int al;
+
+	for (al = 'a'; al <= 'z'; al++)
+		_putchar(al);
+
+	_putchar('\n');
+}
