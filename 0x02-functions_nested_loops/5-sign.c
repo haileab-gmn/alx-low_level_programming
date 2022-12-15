893789fda33ae4fbeaeadf14d13a205265f40b56
haileab-gmn <haileabgebremedhin64@gmail.com>
2022-12-14 16:15:42 -0800

    else if

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign - print sign of a number
+ * @n: the character in question
+ * Return: 1 for greater thant zero, zero for zero and -1 for less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	return (-1);
+}
