6d665f6e7a57150e892cf3ba5a79e86b4e3bb5bf
haileab-gmn <haileabgebremedhin64@gmail.com>
2022-12-20 10:59:51 -0800

    task swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of one for another
+ * @a: int a
+ * @b: int b
+ * Return: result
+ */
+void swap_int(int *a, int *b)
+{
+	int s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
