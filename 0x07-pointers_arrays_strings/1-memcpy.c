a44162f5482a4390ea3564544e48f71de90eec13
haileab-gmn <haileabgebremedhin64@gmail.com>
2023-01-03 11:08:29 -0800

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies a memory ares
+ * @dest: memory where it is stored
+ * @src: memory where it is copied from
+ * @n: number of bytes
+ *
+ * Return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+	int d = n;
+	for (;i < d; i++)
+	{
+		dest[i] = src[i];
+		n--;
+	}
+	return (dest);
+}
