0b047ca0b9564fafaee2c4f2b28be03d92460a27
haileab-gmn <haileabgebremedhin64@gmail.com>
2023-01-05 15:26:51 -0800

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  sqrt - Makes possible to evaluate from 1 to n
+ *   @a: same number as n
+ *   @b: number that iterates from 1 to n
+ *   Return: On success 1.
+ */
+int sqrt(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt(a, b + 1));
+}
+/**
+ *  _sqrt_recursion - returns the natural square root of n
+ *  @n: Number Integer
+ *
+ *  Return: On success 1.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n, 1));
+}
