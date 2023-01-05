d20d74e0f24005d428fd4d32eec621166efe4818
haileab-gmn <haileabgebremedhin64@gmail.com>
2023-01-05 15:29:36 -0800

    sqquareroot

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- *  sqrt - Makes possible to evaluate from 1 to n
+ *  _sqrt - Makes possible to evaluate from 1 to n
  *   @a: same number as n
  *   @b: number that iterates from 1 to n
  *   Return: On success 1.
  */
-int sqrt(int a, int b)
+int _sqrt(int a, int b)
 {
 	if (b * b == a)
 		return (b);
 	else if (b * b > a)
 		return (-1);
-	return (sqrt(a, b + 1));
+	return (_sqrt(a, b + 1));
 }
 /**
  *  _sqrt_recursion - returns the natural square root of n
@@ -22,5 +22,5 @@ int sqrt(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	return (_sqrt(n, 1));
 }
